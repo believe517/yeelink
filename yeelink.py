03bc04dd35f0a0a8edd2d1d6ac4905dee729d612
Shang Shuai <shangshuai517@126.com>
2015-08-05 13:50:12 +0800

    edit yeelink.py

--- yeelink.py
+++ yeelink.py
@@ -7,3 +7,4 @@ while True:
     os.system('sudo fswebcam --no-banner -r 640x480 /home/pi/yeelink.jpg')
     os.system('sudo curl --request POST --data-binary @"/home/pi/yeelink.jpg" --header "U-ApiKey: 1896c43cef112d1ab0002d9d136a17e1" --url http://api.yeelink.net/v1.0/device/89596/sensor/123217/photos')
     time.sleep(30)
+    time.sleep(30)
